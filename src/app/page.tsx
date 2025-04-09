8bf0e2262680c7dac95a16a1449310ab025b7554
anastynero <anastasia1995ggs@gmail.com>
2025-04-09 18:59:35 +0300

    feat: add Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 
+import Hero from "@/components/Hero/Hero";
 import RootLayout from "./layout";
 
 
 export default function Home() {
   return (
   <div>
-
+    <Hero/>
   </div>
   );
 }
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,29 @@
+import styles from './Hero.module.css';
+import Image from 'next/image';
+import vector from "./../../../public/img/Vector.svg";
+
+export default function Hero(){
+    return(
+        <section className="hero">
+            <h1 className={styles.h1}>Издательство
+            новых медиа</h1>
+            <div className={styles.wrapper}>
+            <div className={styles.block}>
+                <h2 className={styles.h2}>100 000 000</h2>
+                <p className={styles.text}>НАША АУДИТОРИЯ</p>
+            </div>
+            <div className={styles.block}>
+                <h2 className={styles.h2}>12 000 000</h2>
+                <p className={styles.text}>ЕЖЕДНЕВНО НАС ЧИТАЮТ</p>
+            </div>
+            </div>
+            <Image
+                src={vector}
+                alt="Стрелка вниз"
+                width={24}
+                height={24}
+                className={styles.vector}
+            />
+        </section>
+    );
+}
