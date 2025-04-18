31c875748efd15ea318524cd49bacae2beebe0c3
anastynero <anastasia1995ggs@gmail.com>
2025-04-18 14:53:26 +0300

    fix: fix og property

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,20 +4,6 @@ import Footer from "@/components/Footer/Footer";
 import './global.css';
 import { Providers } from './providers';
 
-export const metadata: Metadata = {
-  title: "Wolfmedia",
-  description: "«Wolfmedia» – это редакция, где ежедневно авторы, иллюстраторы, дизайнеры, видеографы, создают уникальный контент и доносят его до огромной аудитории с помощью социальных сетей.",
-  openGraph: {
-    images: [
-      {
-        url: 'http://localhost:3000/img/logo-light.svg', 
-      },
-    ],
-    locale: 'ru_RU',
-    type: 'website',
-  }
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
