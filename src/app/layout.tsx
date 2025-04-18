66760cc5459e92ddcafb43526717946c00a3f159
anastynero <anastasia1995ggs@gmail.com>
2025-04-18 15:02:00 +0300

    fix: fix bugs with responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,18 +12,16 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-      <meta property="og:title" content="Wolfmedia"/>
-      <meta property="og:description" content="«Wolfmedia» – это редакция, где ежедневно 
-      авторы, иллюстраторы, дизайнеры, видеографы, создают уникальный контент и доносят его 
-      до огромной аудитории с помощью социальных сетей."/>
-      <meta property="og:locale" content="ru_RU"/>
-      <meta property="og:type" content="website"/>
-      <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      <link rel="manifest" href="/site.webmanifest" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+        <meta property="og:title" content="Wolfmedia"/>
+        <meta property="og:description" content="«Wolfmedia» – это редакция, где ежедневно 
+        авторы, иллюстраторы, дизайнеры, видеографы, создают уникальный контент и доносят его 
+        до огромной аудитории с помощью социальных сетей."/>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>
         <Header/>
